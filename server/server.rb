90eff4b4eef818fc30261f5afb30537268eca153
Will <wor101@gmail.com>
2021-04-08 15:02:12 -0400

    dice server with html added

--- server/server.rb
+++ server/server.rb
@@ -2,24 +2,14 @@ require "socket"  # need to create
 
 server = TCPServer.new("localhost", 3003)   #create TCP server on localhost and port #
 
-def get_http_method(request_line)
-  request_line.split[0]
-end
-
-def get_path(request_line)
-  request_line.split[1].split("?")[0]  
-end
-
-def get_params(request_line)
-  path_and_query_string = request_line.split[1]
-  query_string = path_and_query_string.split("?")[1]
-  params = {}
-  
-  query_string.split("&").each do |key_value|
-    key = key_value.split("=")[0]
-    value = key_value.split("=")[1]
-    params[key] = value
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split
+  path, params = path_and_params.split("?")
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
   end
+  [http_method, path, params]
 end
 
 loop do
@@ -28,20 +18,31 @@ loop do
   request_line = client.gets  # retrieves first line of the request text
   next if !request_line || request_line =~ /favicon/
   puts request_line
-  
+ 
+  http_method, path, params = parse_request(request_line)
 
-# GET /?rolls=2&sides=6 HTTP/1.1
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
 
-
-  http_method = get_http_method(request_line)
-  path = get_path(request_line)
-  params = get_params(request_line)
-  puts params
-
-  client.puts "HTTP/1.1 200 OK\r\n\r\n"
-  client.puts request_line
-  params["rolls"].to_i.times do 
-    client.puts rand(params["sides"].to_i) + 1
+  client.puts "<h1>Rolls!</h1>"
+  rolls = params["rolls"].to_i
+  sides = params["sides"].to_i
+  rolls.times do 
+    roll = rand(sides) + 1
+    client.puts "<p>", roll, "</p>"
   end
+
+  client.puts "</body>"
+  client.puts "</html>"
+
   client.close
 end
+
