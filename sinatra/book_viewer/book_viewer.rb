8d592292d3e1871d285f6fd36b21330947adcb6c
Will <wor101@gmail.com>
2021-04-12 17:00:03 -0400

    start of book viewer created with Sinatra

---
+++ sinatra/book_viewer/book_viewer.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+
+
+get "/" do
+  @title = "The Adventures of CatMan"
+  @contents = File.readlines "data/toc.txt"
+
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines "data/toc.txt"
+  @chapter = File.read "data/chp1.txt"
+
+  erb :chapter
+end
